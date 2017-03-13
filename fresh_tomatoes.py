7903b33fed9d6bf9c6a445a57e54f94bc4e8623e
Ravali Ginjupalli <ravali.ginjupalli@gmail.com>
2017-03-13 12:55:20 -0500

    fix: Bug fix in youtube url

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -136,8 +136,10 @@ def create_movie_tiles_content(movies):
             r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(
             r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+
         trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
                               else None)
+        print(trailer_youtube_id)
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
